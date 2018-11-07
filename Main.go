f4283bb0e56dc022bed4ff1d5167eff8abd1fa41
usbpc <spieler3001@googlemail.com>
2018-11-07 20:53:57 +0100

    Getting upload URL is working.

--- Main.go
+++ Main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-"encoding/json"
-"fmt"
-"io/ioutil"
-"log"
-"net/http"
-	"net/http/cookiejar"
+	"encoding/json"
+	"fmt"
+	"google.golang.org/api/googleapi"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
 
-"golang.org/x/net/context"
-"golang.org/x/oauth2"
-"golang.org/x/oauth2/google"
-"google.golang.org/api/drive/v3"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v3"
 )
 
 // Retrieve a token, saves the token, then returns the generated client.
@@ -85,48 +87,129 @@ func main() {
 		log.Fatalf("Unable to retrieve Drive client: %v", err)
 	}
 
-	rsp, err := srv.Files.Get("root").Do()
-	if err != nil {
-		log.Fatalf("Well that went badly: %v", err)
-	}
-	cookiejar.New(nil)
-	fmt.Println(rsp.Id)
+	ChunckedUpload(&File{
+		r: nil,
+		f: &drive.File{
+			Name: "Test",
+		},
+		size: int64(10),
+	}, srv, client)
+}
 
-	r, err := srv.Files.List().
-		SupportsTeamDrives(true).
-		IncludeTeamDriveItems(true).
-		Q("mimeType='application/vnd.google-apps.folder'").
-		PageSize(1000).Fields("files(id, name, parents)").Do()
-	if err != nil {
-		log.Fatalf("Unable to retrieve files: %v", err)
+
+func initUpload(f *File, client *http.Client) (string, error) {
+	//URL query parameters
+	query := url.Values{
+		"uploadType": {"resumable"},
+		"supportsTeamDrives": {"true"},
 	}
-	fmt.Println("Files:")
-	if len(r.Files) == 0 {
-		fmt.Println("No files found.")
-	} else {
-		for _, i := range r.Files {
-			fmt.Printf("%s (%s)\n", i.Name, i.Id)
+
+	//Base url
+	urls := "https://www.googleapis.com/upload/drive/v3/files"
+
+	//Let's add all query parameters to the url
+	urls += "?" + query.Encode()
+
+	//Create a body from the file
+	body, err := googleapi.WithoutDataWrapper.JSONReader(f.f)
+	if err != nil {
+		return "", err
+	}
+
+	//New request
+	req, err := http.NewRequest("POST", urls, body)
+	if err != nil {
+		return "", err
+	}
+
+	//Let's be nice and tell google how big the file we'll upload is.
+	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	req.Header.Set("X-Upload-Content-Length", fmt.Sprintf("%v", f.size))
+
+	//And let's finally do the request to get the actual URL that we'll use to upload things.
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	err = googleapi.CheckResponse(res)
+	defer googleapi.CloseBody(res)
+	if err != nil {
+		return "", err
+	}
+
+	return res.Header.Get("Location"), nil
+}
+
+func ChunckedUpload(f *File, srv *drive.Service, client *http.Client) (string, error)  {
+
+
+	url, err := initUpload(f, client)
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Printf("I got an upload url: %s", url)
+
+	/**
+	reader := &MyReader{
+		r: f.r,
+		buf: make([]byte, 256 * 1024), //Currently just using min size according to: https://developers.google.com/drive/api/v3/resumable-upload
+	}
+
+	send := int64(0)
+
+	for send < f.size {
+		req, err := http.NewRequest("PUT", urls, reader)
+		if err != nil {
+			log.Fatalf("Well that didn't go as expected: %v", err)
 		}
+		req.Header.Set("Content-Range", fmt.Sprintf("%v-%v/%v", send, f.size-send, f.size))
+		res, err := client.Do(req)
+		if err != nil {
+			log.Fatalf("Request didn't go as expected: %v", err)
+		}
+		err = googleapi.CheckResponse(res)
+
+		if err != nil {
+			log.Fatalf("Why you hate me google? %v", err)
+		}
+
+	}**/
+
+	return "", nil
+}
+
+type MyReader struct {
+	r io.Reader
+	buf []byte
+	pos int
+}
+
+func (r *MyReader) Seek(offset int64, whence int) (int64, error) {
+	switch whence {
+	case io.SeekStart: {}
+	case io.SeekCurrent: {}
+	case io.SeekEnd: {}
 	}
 
-	fmt.Println("Trying to upload a thing!")
+	return 0, nil
+}
 
-	response, err := http.Get("https://speed.hetzner.de/100MB.bin")
-	if err != nil {
-		log.Fatalf("Something went very wrong %v", err)
-	}
-	defer response.Body.Close()
+func (r *MyReader) Read(p []byte) (n int, err error) {
+	n = copy(p, r.buf[r.pos:])
+	r.pos += n
+	return n, nil
+}
 
-	file := drive.File{
-		Name: "100MB.bin",
-		TeamDriveId: "0AGZqTq8YYmQ7Uk9PVA",
-		Parents: []string{"0AGZqTq8YYmQ7Uk9PVA"},
-	}
+type File struct {
+	r io.Reader
+	f *drive.File
+	size int64
+}
 
-	created, err := srv.Files.Create(&file).Media(response.Body).SupportsTeamDrives(true).Do()
-	if err != nil {
-		log.Fatalf("Well this is akward: %v", err)
-	}
-
-	fmt.Printf("The file has the id %s", created.Id)
+type Host interface {
+	Login(usr string, pwd string) bool
+	File(url string, pwd string) File
+	Folder(url string) (urls []string)
 }
